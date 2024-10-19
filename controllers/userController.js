9f9b424307bcdcca3603e848729cd9c66e718112
Shakil57375 <shakil57375@gmail.com>
2024-10-19 17:27:15 +0600

    user modal added successfully

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+// route for user login 
+const loginUser = async (req, res) => {
+
+};
+// route for user registration
+const registerUser = async (req, res) => {
+
+};
+
+// route for admin login
+const adminLogin = async (req, res) => {
+    // authenticate admin credentials
+};
+
+export { loginUser, registerUser, adminLogin};
