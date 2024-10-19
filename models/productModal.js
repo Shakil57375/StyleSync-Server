e9b2830b35c1ffc6ed0231f8d6c31816d74df059
Shakil57375 <shakil57375@gmail.com>
2024-10-19 17:06:12 +0600

    product modal added successfully

---
+++ models/productModal.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  sizes: {
+    type: Array,
+    required: true,
+  },
+  bestseller: { type: "boolean" },
+  latestProduct: { type: "boolean" },
+  date: { type: Number, required: true }, 
+});
+
+const productModal = mongoose.models.product || mongoose.model("product", productSchema);
+export default productModal;
