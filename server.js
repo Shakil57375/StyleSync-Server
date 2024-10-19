64c6371bbe485a1a1b83bdaa4355c8a528ceef7a
Shakil57375 <shakil57375@gmail.com>
2024-10-19 12:35:05 +0600

    successfully connected to the mongodb

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import "dotenv/config";
+
+// App Configuration
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware configuration
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection URI
+const uri = "mongodb+srv://StyleSync:Fwaqvaw8myKSUzwc@cluster0.lc40fqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close(); // Uncomment this if you want to close the connection after finishing
+  }
+}
+run().catch(console.dir);
+
+// API endpoints
+app.get("/", (req, res) => {
+    res.send("API working");
+})
+
+app.listen(port, () => console.log("server listening on port", port));
