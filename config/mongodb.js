5f5dc54dd3055c1b24c5a7eead15761d275ba074
Shakil57375 <shakil57375@gmail.com>
2024-10-27 11:50:59 +0600

    user data stored in mongodb

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,14 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connection.on("connected", () => {
-      console.log("Mongoose connection established successfully");
-    });
-    
-    await mongoose.connect(`${process.env.MONGODB_URI}/StyleSync`);
-
+    await mongoose.connect(process.env.MONGODB_URI);  // No additional options needed
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("MongoDB connection error:", error);
   }
 };
 
