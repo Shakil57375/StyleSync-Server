89896c36b4f9a2f4ac6dd391f9f7e4aff1f8eebf
Shakil57375 <shakil57375@gmail.com>
2024-10-27 12:17:17 +0600

    user data stored in mongodb

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,56 +10,75 @@ const createToken = (id) => {
 };
 
 // route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User Doesn't exist please login",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 // route for user registration
 const registerUser = async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
-  
-      // check if user already exists
-      const existingUser = await userModel.findOne({ email });
-      if (existingUser) {
-        return res
-          .status(400)
-          .json({ success: false, message: "User already exists" });
-      }
-  
-      // validating email address and strong password
-      if (!validator.isEmail(email)) {
-        return res
-          .status(400)
-          .json({ success: false, message: "Invalid email address" });
-      }
-  
-      const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#+\-_=<>?])[A-Za-z\d@$!%*?&#+\-_=<>?]{8,}$/;
-      if (!passwordRegex.test(password)) {
-        return res.status(400).json({
-          success: false,
-          message:
-            "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-        });
-      }
-  
-      // hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-  
-      // create new user
-      const newUser = new userModel({ name, email, password: hashedPassword });
-      const savedUser = await newUser.save();
-  
-      const token = createToken(savedUser._id);
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user already exists
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
       return res
-        .status(201)
-        .json({ success: true, token, message: "User registered successfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: "Server error" });
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
-  };
-  
-  
+
+    // validating email address and strong password
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email address" });
+    }
+
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#+\-_=<>?])[A-Za-z\d@$!%*?&#+\-_=<>?]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create new user
+    const newUser = new userModel({ name, email, password: hashedPassword });
+    const savedUser = await newUser.save();
+
+    const token = createToken(savedUser._id);
+    return res
+      .status(201)
+      .json({ success: true, token, message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
 
 // route for admin login
 const adminLogin = async (req, res) => {
