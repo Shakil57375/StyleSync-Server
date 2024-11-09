db845168f7443812dabf5b02eaea678cbcf5e17e
Shakil57375 <shakil57375@gmail.com>
2024-11-09 06:54:56 -0800

    updated

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,6 @@ import { loginUser, registerUser, adminLogin } from '../controllers/userControll
 const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/admin", adminLogin);
+userRouter.post("/admin", adminLogin );
 
 export default userRouter;
